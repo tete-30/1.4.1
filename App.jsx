a3dc1511b30641c0b28000fb2510911d67bc32df
tete-30 <112039750+tete-30@users.noreply.github.com>
2023-05-19 22:20:56 -0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,65 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+const filmes = [
+  {
+    nome: 'O Senhor do Aneis: A Sociedade do Anel',
+    lancamento: 2002,
+    oscar: true,
+    diretores: ['Peter Jackson'],
+    generos: ['Fantasia', 'Aventura'],
+  },
+  {
+    nome: 'Harry Potter e a Pedra Filosofal',
+    lancamento: 2001,
+    oscar: true,
+    diretores: ['Chris Columbus'],
+    generos: ['Fantasia'],
+  },
+  {
+    nome: 'Matrix',
+    oscar: true,
+    lancamento: 1999,
+    diretores: ['Lana Wachowski', 'Lilly Wachowski'],
+    generos: ['Ação', 'Ficção Cientifica'],
+  },
+  {
+    nome: 'Meninas Malvadas',
+    oscar: false,
+    lancamento: 2004,
+    diretores: ['Mark Waters'],
+    generos: ['Comédia'],
+  }
+];
+
+function Filme({ nome, diretores }) {
+  return (
+    <div>
+      <h4>{nome}</h4>
+      <ul>{diretores.map((diretor) => (
+        <li>{diretor}</li>
+      ))}
+      </ul>
+    </div>
+  )
+}
+
+
+function App() {
+  return (
+    <div>
+
+      {filmes.map(filme => (
+      <Filme diretores={filme.diretores} nome={filme.nome} />
+      ))}
+
+    </div>
+  );
+
+}
+
+export default App
+
